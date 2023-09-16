6dc3e33dc8b659a1ede140bc7fcf678b280a9bbd
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-16 11:40:43 +0200

    premier fenetre java

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Snake");
+        frame.setSize(12*50, 12*50);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
