01abb570b9d69b84730091e26e3f9e960e1061e0
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-16 11:56:33 +0200

    creation des deplacements dans la class part

--- Main.java
+++ Main.java
@@ -151,4 +151,35 @@ public class Main extends JPanel{
 			}
 		}
 	}
+    class SnakePart {
+		
+		public int x, y, direction;
+		
+		public SnakePart(int x, int y, int direction) {
+			this.x = x;
+			this.y = y;
+			this.direction = direction;
+		}
+		
+		public void move() {
+			if(direction == 37 || direction == 39) {
+				x += (direction == 37) ? -1 : 1;
+				if(x > 13)
+					x = -1;
+				else if(x < -1)
+					x = 13;
+			}else {
+				y += (direction == 38) ? -1 : 1;
+				if(y > 13)
+					y = -1;
+				else if(y < -1)
+					y = 13;
+			}
+		}
+		
+		@Override
+		protected Object clone() throws CloneNotSupportedException {
+			return new SnakePart(x, y, direction);
+		}
+	}
 }
